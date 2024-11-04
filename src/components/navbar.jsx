e04404d8c986b89030f4f9dd32ca9dadc6d8e95c
jeweler <valve.valve101@gmail.com>
2024-11-03 23:07:06 -0500

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 text-white p-3 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="font-bold text-lg">Local Event Finder</div>
+                <ul className="flex space-x-4">
+                    <li>
+                        <button className="bg-blue-500 hover:bg-blue-600
+                            text-white font-semibold py-1 px-2 rounded-sm">
+                            Login
+                        </button>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-blue-400">
+                            About
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,11 +1,13 @@
+import Navbar from "../components/navbar"
 import "../index.css"
 
 const Home = () => {
     return (
         <>
+            <Navbar />
             <div className="flex flex-row items-center justify-center py-10">
                 <h1 className="text-xl font-test">
-                    Git pull test
+                    Welcome to Local Event Finder
                 </h1>
             </div>
         </>
